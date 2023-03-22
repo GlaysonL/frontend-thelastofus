d746ebdf411aecf3e31c205b6dedef94beed4953
GlaysonL <glayson.leo@gmail.com>
2023-03-21 23:34:32 -0300

    ADD: HTML + CSS(estilo e reset)

---
+++ source/js/index.js
@@ -0,0 +1,15 @@
+/*
+
+1 - Capturar o elemento html dos botoes
+
+2 - Identificar o clique do mouse
+
+3 - Desmarcar o botão selecionado anteriormente
+
+4 - Marcar o botão clicado
+
+5 - Esconder a imagem de fundo anterior
+
+6 - Atualizar a imagem para o novo botao selecionado
+
+*/
