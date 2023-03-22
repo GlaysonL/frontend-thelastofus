972f91d393f0d55750d457777eb91d1166adebcd
GlaysonL <glayson.leo@gmail.com>
2023-03-22 00:38:42 -0300

    ADD: JS

--- source/js/index.js
+++ source/js/index.js
@@ -13,3 +13,36 @@
 6 - Atualizar a imagem para o novo botao selecionado
 
 */
+
+//  Objetivo 1
+const botoesCarrosel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+// Objetivo 2
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        // Objetivo 3
+        desativarBotaoSelecionado();
+
+        // Objetivo 4
+        botao.classList.add('selecionado');
+
+
+        //objetivo 5
+        esconderImagemAtiva();
+
+        //objetivo 6 
+        imagens[indice].classList.add('ativa');
+    })
+})
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
